4c2373355a98e6af3964aac2ea6872f7e3e46589
Vladislav Semenets <vladislav.sem@computools.com>
2024-04-08 15:13:02 +0300

    feat: added connect wallet modal basic structure to UI-kit

--- components/ui/select-currency.tsx
+++ components/ui/select-currency.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { ChevronDown, ChevronUp, Wallet } from 'lucide-react';
 import { useState } from 'react';
 import { Select, SelectContent, SelectGroup, SelectIcon, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
