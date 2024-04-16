9f901d9ef389ef54abe444d32098e4d7da027aa4
Vladislav Semenets <vladislav.sem@computools.com>
2024-04-16 12:50:19 +0300

    feat: improved area chart, customized tooltip

--- components/ui/charts/axone-area-chart.tsx
+++ components/ui/charts/axone-area-chart.tsx
@@ -1,9 +1,11 @@
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { Area, AreaChart, Brush, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { ChartData } from '@/app/mock-chart-data';
 import ChartTooltip from './chart-tooltip';
 import ChartTraveler from './chart-traveller';
 import Column from '../column';
+import Spinner from '../spinner';
+import { formatChartDate } from '@/lib/utils';
 
 // Override console.error
 // This is a hack to suppress the warning about missing defaultProps in recharts library as of version 2.12
@@ -20,42 +22,71 @@ type AxoneAreaChartProps = {
 };
 
 const CHART_COLOR = '#FB9501';
+const CHART_MARGIN = {
+  top: 10,
+  right: 30,
+  left: 0,
+  bottom: 0,
+};
 
 const AxoneAreaChart = ({ data }: AxoneAreaChartProps) => {
+  const [isLoading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setLoading(false);
+    }
+  }, [data]);
+
   return (
     <Column>
       <ResponsiveContainer width='100%' height='100%'>
-        <AreaChart
-          data={data}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <defs>
-            <linearGradient id='colorUv' x1='0' y1='0' x2='0' y2='1'>
-              <stop offset='0%' stopColor={CHART_COLOR} stopOpacity={1}/>
-              <stop offset='100%' stopColor={CHART_COLOR} stopOpacity={0}/>
-            </linearGradient>
-            <linearGradient id='colorUv2' x1='0' y1='0' x2='0' y2='1'>
-              <stop offset='0%' stopColor={CHART_COLOR} stopOpacity={0.2}/>
-              <stop offset='100%' stopColor={CHART_COLOR} stopOpacity={0}/>
-            </linearGradient>
-          </defs>
-          <XAxis dataKey='name' axisLine={false} className='select-none' tick={{ fontSize: 14 }} tickMargin={5} height={40}/>
-          <YAxis axisLine={false} className='select-none' tick={{ fontSize: 14 }} tickMargin={5} />
-          <Tooltip content={<ChartTooltip />} />
-          <Area type='linear' dot={<circle r={4} fill={CHART_COLOR} stroke='none' fillOpacity={1} />} dataKey='price' stroke={CHART_COLOR} fill='url(#colorUv)' />
-          <Brush dataKey='name' stroke='transparent' traveller={ChartTraveler} height={36} fill='transparent' >
-            <AreaChart
-              data={data}
-            >
-              <Area type='linear' dataKey='price' stroke={CHART_COLOR}  fillOpacity={1} fill='url(#colorUv2)' />
-            </AreaChart>
-          </Brush>
-        </AreaChart>
+        {
+          isLoading
+            ? (<div className='w-full h-full flex flex-col justify-center items-center '>
+              <Spinner />
+            </div>)
+            : (
+              <AreaChart
+                data={data}
+                margin={CHART_MARGIN}
+              >
+                <defs>
+                  <linearGradient id='colorUv' x1='0' y1='0' x2='0' y2='1'>
+                    <stop offset='0%' stopColor={CHART_COLOR} stopOpacity={1}/>
+                    <stop offset='100%' stopColor={CHART_COLOR} stopOpacity={0}/>
+                  </linearGradient>
+                  <linearGradient id='colorUv2' x1='0' y1='0' x2='0' y2='1'>
+                    <stop offset='0%' stopColor={CHART_COLOR} stopOpacity={0.2}/>
+                    <stop offset='100%' stopColor={CHART_COLOR} stopOpacity={0}/>
+                  </linearGradient>
+                </defs>
+                <XAxis
+                  dataKey='time'
+                  axisLine={false}
+                  tick={{ fontSize: 14 }}
+                  className='select-none'
+                  tickMargin={2}
+                  tickFormatter={formatChartDate}
+                />
+                <YAxis axisLine={false} className='select-none' tick={{ fontSize: 14 }} tickMargin={5} />
+                <Tooltip
+                content={({ active, payload }) => {
+                  const formattedDate = formatChartDate(payload?.[0]?.payload.time, true);
+                  return <ChartTooltip active={active} payload={payload} formattedDate={formattedDate} />;
+                }}
+                />
+                <Area type='linear' dot={<circle r={4} fill={CHART_COLOR} stroke='none' fillOpacity={1} />} dataKey='price' stroke={CHART_COLOR} fill='url(#colorUv)' />
+                <Brush dataKey='name' stroke='transparent' traveller={ChartTraveler} height={36} fill='transparent' >
+                  <AreaChart
+                    data={data}
+                  >
+                    <Area type='linear' dataKey='price' stroke={CHART_COLOR}  fillOpacity={1} fill='url(#colorUv2)' />
+                  </AreaChart>
+                </Brush>
+              </AreaChart>
+            )
+        }
       </ResponsiveContainer>
     </Column>
   );
--- components/ui/charts/chart-tooltip.tsx
+++ components/ui/charts/chart-tooltip.tsx
@@ -1,3 +1,4 @@
+import Row from '../row';
 
 
 type TooltipPayload = {
@@ -8,18 +9,26 @@ type TooltipProps = {
   active?: boolean;
   payload?: TooltipPayload;
   label?: string;
+  formattedDate?: string;
 }
 
 /**
  * ChartTooltip component
  * Represents the tooltip in the chart
  */
-const ChartTooltip = ({ active, payload, label }: TooltipProps) => {
+const ChartTooltip = ({ active, payload, formattedDate }: TooltipProps) => {
   if (active && payload && payload.length) {
     return (
-      <div className='custom-tooltip'>
-        <p className='label'>{`Date : ${label}`}</p>
-        <p className='intro'>{`Price : ${payload[0].value}`}</p>
+      <div className='bg-axone-dark-blue rounded-md p-1 custom-tooltip'>
+        <p className='rounded-t-md p-2 bg-axone-bg-dark mb-2'>{formattedDate}</p>
+        <Row className='px-4'>
+          <div className='w-5 h-5 bg-axone-orange rounded-full' />
+          <p className='text-axone-white ml-2'><span className='text-axone-khaki'>Price:</span> ${Math.floor(payload[0].value).toFixed(2)}</p>
+        </Row>
+        <Row className='px-4 mb-4'>
+          <div className='w-5 h-5 bg-axone-dark-blue rounded-full' />
+          <p className='text-axone-white ml-2'><span className='text-axone-khaki'>Supplier:</span> AXONE</p>
+        </Row>
       </div>
     );
   }
