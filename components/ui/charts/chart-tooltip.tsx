3871fb41ff4b0e77db14e88bf4ca5217ec42eac9
Vladislav Semenets <vladislav.sem@computools.com>
2024-04-16 13:16:38 +0300

    fix: solved issue with chart tooltip types

--- components/ui/charts/chart-tooltip.tsx
+++ components/ui/charts/chart-tooltip.tsx
@@ -1,13 +1,9 @@
+import { NameType, Payload, ValueType } from 'recharts/types/component/DefaultTooltipContent';
 import Row from '../row';
-
-
-type TooltipPayload = {
-  value: number;
-}[];
 
 type TooltipProps = {
   active?: boolean;
-  payload?: TooltipPayload;
+  payload?: Payload<ValueType, NameType>[] | undefined ;
   label?: string;
   formattedDate?: string;
 }
@@ -17,13 +13,14 @@ type TooltipProps = {
  * Represents the tooltip in the chart
  */
 const ChartTooltip = ({ active, payload, formattedDate }: TooltipProps) => {
+  const value = payload?.[0]?.value;
   if (active && payload && payload.length) {
     return (
       <div className='bg-axone-dark-blue rounded-md p-1 custom-tooltip'>
         <p className='rounded-t-md p-2 bg-axone-bg-dark mb-2'>{formattedDate}</p>
         <Row className='px-4'>
           <div className='w-5 h-5 bg-axone-orange rounded-full' />
-          <p className='text-axone-white ml-2'><span className='text-axone-khaki'>Price:</span> ${Math.floor(payload[0].value).toFixed(2)}</p>
+          <p className='text-axone-white ml-2'><span className='text-axone-khaki'>Price:</span> ${value}</p>
         </Row>
         <Row className='px-4 mb-4'>
           <div className='w-5 h-5 bg-axone-dark-blue rounded-full' />
